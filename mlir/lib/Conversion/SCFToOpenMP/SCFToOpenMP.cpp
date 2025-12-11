b2c8d34654d6982426117aaac113341f77925532
skc7 <Krishna.Sankisa@amd.com>
2025-12-11 11:56:58 +0530

    [OpenMP][MLIR] Add num_threads clause with dims modifier support

--- mlir/lib/Conversion/SCFToOpenMP/SCFToOpenMP.cpp
+++ mlir/lib/Conversion/SCFToOpenMP/SCFToOpenMP.cpp
@@ -448,6 +448,8 @@ struct ParallelOpLowering : public OpRewritePattern<scf::ParallelOp> {
         /* allocate_vars = */ llvm::SmallVector<Value>{},
         /* allocator_vars = */ llvm::SmallVector<Value>{},
         /* if_expr = */ Value{},
+        /* num_threads_dims = */ nullptr,
+        /* num_threads_values = */ llvm::SmallVector<Value>{},
         /* num_threads = */ numThreadsVar,
         /* private_vars = */ ValueRange(),
         /* private_syms = */ nullptr,
